e3cc57837451024b3058df7888edcfc4924e45f6
hikmat farhat <hfarhat@ndu.edu.lb>
2021-02-22 14:04:59 +0200

    update

--- stl/stl.cpp
+++ stl/stl.cpp
@@ -5,12 +5,13 @@
 #include <vector>
 #include <list>
 
-template<typename Ostream,typename Container>
+template <typename Ostream,typename Container>
 Ostream& operator<<(Ostream& os,Container& c) {
 	for (auto x : c)
 		os<< x << ",";
 	return os;
 }
+
 /* template specialization */
 template <>
 std::ostream& operator<< <std::ostream, std::string>
@@ -22,17 +23,31 @@ std::ostream& operator<< <std::ostream, std::string>
 
 
 //#define REMOVE_ERASE
-#define SEARCH
-//#define INCLUDES
+//#define SEARCH
+#define INCLUDES
+struct Test {
+
+};
 
 #ifdef REMOVE_ERASE
 int main()
 {
+	int y = 6;
+	
+	std::vector< int > v{ 1,2,3,2,6,2,4,2 };
 	std::string s{ "Name, Mid,dle, Last" };
-	auto itr = std::remove(s.begin(), s.end(), ',');
-	std::cout << s<<"\n";
-	s.erase(itr, s.end());
-	std::cout << s<<"\n";;
+	//auto itr = std::remove(s.begin(), s.end(), ',');
+	auto itr = std::remove_if(v.begin(), v.end(),
+		[](int x) {
+			return (x % 2 == 0);
+		}
+	);
+		
+	std::cout << v;
+	std::cout << "\n";
+	v.erase(itr, v.end());
+	std::cout << v;
+	std::cout << "\n";
 }
 #endif // REMOVE_ERASE
 
@@ -40,10 +55,12 @@ int main()
 int main(){
 	std::string s{ "First Middle Last" };
 
-	bool exists=s.find("Midle")!=std::string::npos;
+	//bool exists=s.find("Midle")!=std::string::npos;
 	std::string sub = "Middle";
-	auto subitr = std::search(s.begin(), s.end(), sub.begin(), sub.end());
-	exists = subitr != s.end();
+
+	/*auto subitr = std::search(s.begin(), s.end(), sub.begin(), sub.end());
+	exists = subitr != s.end();*/
+
 	std::vector<int> v{ 4,2,7,3,1,5 };
 	std::reverse(v.begin(), v.end());
 	std::cout<<v<<"\n";
@@ -58,9 +75,20 @@ int main(){
 }
 #endif // SEARCH
 
-#ifdef INCLUDE
+#ifdef INCLUDES
+#include <memory>
+struct NoCopy {
+
+};
+int main(){
+	std::vector<int> v{ 8,4,9,29,6 };// {4,6,8,9,29}
+	std::sort(v.begin(), v.end());
+	std::vector<int> needle{ 6,10};
 	/* both ranges must be ordered to use include */
-		//std::cout<<std::includes(v.begin(), v.end(), needle.begin(), needle.end());
+	std::cout<<std::includes(v.begin(), v.end(), needle.begin(), needle.end());
+	std::unique_ptr<int> p, q;
+	p = std::move(q);
+	}
 #endif // INCLUDE
 
 
